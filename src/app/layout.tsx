51ca7687ccd2903955372dec9d13832c10d30884
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2026-02-21 16:32:53 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import './globals.scss';
+import {Analytics} from '@vercel/analytics/next';
 
 const inter = Inter({subsets: ['latin']});
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <body className={inter.className}>
         {children}
         </body>
+        <Analytics/>
         </html>
     );
 }
